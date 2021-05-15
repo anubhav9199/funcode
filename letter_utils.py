578c1d80e66820d78377ae176e1a29533d02fda4
Anubhav Sharma <anubhav.sharma@medcords.com>
2021-05-15 15:36:03 +0530

    adding letter utils file ready for Anubhav word

---
+++ letter_utils.py
@@ -0,0 +1,221 @@
+def a_function(turtle, letter):
+    if letter == 'A':
+        turtle.forward(30)
+        turtle.right(45)
+        turtle.forward(30)
+        turtle.right(90)
+        turtle.forward(30)
+        turtle.right(135)
+        turtle.forward(40)
+        turtle.penup()
+        turtle.backward(40)
+        turtle.pendown()
+        turtle.left(90)
+        turtle.forward(30)
+    elif letter == 'a':
+        turtle.penup()
+        turtle.forward(35)
+        turtle.pendown()
+        turtle.right(90)
+        turtle.forward(30)
+        turtle.right(90)
+        turtle.forward(35)
+        turtle.right(90)
+        turtle.forward(30)
+        turtle.right(90)
+        turtle.forward(20)
+        turtle.right(90)
+        turtle.forward(30)
+        turtle.penup()
+        turtle.right(90)
+        turtle.forward(20)
+        turtle.left(45)
+        turtle.forward(5)
+        turtle.left(45)
+        turtle.forward(5)
+        turtle.right(90)
+    else:
+        return
+
+
+def b_function(turtle, letter):
+    if letter == 'B':
+        pass
+    elif letter == 'b':
+        turtle.forward(60)
+        turtle.backward(35)
+        turtle.right(90)
+        turtle.forward(25)
+        turtle.right(90)
+        turtle.forward(25)
+        turtle.right(90)
+        turtle.forward(25)
+        turtle.backward(25)
+        turtle.left(90)
+    else:
+        return
+
+
+def c_function(turtle, letter):
+    pass
+
+
+def d_function(turtle, letter):
+    pass
+
+
+def e_function(turtle, letter):
+    pass
+
+
+def f_function(turtle, letter):
+    pass
+
+
+def g_function(turtle, letter):
+    pass
+
+
+def h_function(turtle, letter):
+    if letter ==  'H':
+        pass
+    elif letter == 'h':
+        turtle.forward(60)
+        turtle.backward(35)
+        turtle.right(90)
+        turtle.forward(25)
+        turtle.right(90)
+        turtle.forward(25)
+    else:
+        return
+
+
+def i_function(turtle, letter):
+    pass
+
+
+def j_function(turtle, letter):
+    pass
+
+
+def k_function(turtle, letter):
+    pass
+
+
+def l_function(turtle, letter):
+    pass
+
+
+def m_function(turtle, letter):
+    pass
+
+
+def n_function(turtle, letter):
+    if letter == 'N':
+        pass
+    elif letter == 'n':
+        turtle.forward(30)
+        turtle.left(90)
+        turtle.forward(10)
+        turtle.backward(10)
+        turtle.right(180)
+        turtle.forward(30)
+        turtle.right(90)
+        turtle.forward(30)
+    else:
+        return
+
+
+def o_function(turtle, letter):
+    pass
+
+
+def p_function(turtle, letter):
+    pass
+
+
+def q_function(turtle, letter):
+    pass
+
+
+def r_function(turtle, letter):
+    pass
+
+
+def s_function(turtle, letter):
+    pass
+
+
+def t_function(turtle, letter):
+    pass
+
+
+def u_function(turtle, letter):
+    if letter == 'U':
+        pass
+    elif letter == 'u':
+        turtle.right(90)
+        turtle.forward(5)
+        turtle.left(45)
+        turtle.forward(5)
+        turtle.left(45)
+        turtle.forward(25)
+        turtle.backward(30)
+        turtle.right(90)
+        turtle.forward(30)
+        turtle.left(90)
+        turtle.forward(30)
+        turtle.backward(25)
+        turtle.right(135)
+        turtle.forward(5)
+        turtle.left(45)
+        turtle.forward(5)
+        turtle.right(90)
+    else:
+        return
+
+
+def v_function(turtle, letter):
+    if letter == 'V':
+        turtle.penup()
+        turtle.forward(55)
+        turtle.right(155)
+        turtle.pendown()
+        turtle.forward(60)
+        turtle.left(125)
+        turtle.forward(60)
+        turtle.right(95)
+        turtle.penup()
+        turtle.forward(55)
+        turtle.pendown()
+        turtle.right(90)
+    elif letter == 'v':
+        turtle.penup()
+        turtle.forward(40)
+        turtle.right(155)
+        turtle.pendown()
+        turtle.forward(40)
+        turtle.left(125)
+        turtle.forward(40)
+        turtle.right(155)
+        turtle.penup()
+        turtle.forward(35)
+        turtle.pendown()
+    else:
+        return
+
+
+def w_function(turtle, letter):
+    pass
+
+
+def x_function(turtle, letter):
+    pass
+
+
+def y_function(turtle, letter):
+    pass
+
+
+def z_function(turtle, letter):
+    pass
