73ad665b164c1a33e426bba881049effdf869d85
Anubhav Sharma <anubhav.sharma@medcords.com>
2021-05-25 17:31:26 +0530

    fixing error states in the vowel and conconent calculator

--- count_vowel_and_consonent.py
+++ count_vowel_and_consonent.py
@@ -3,18 +3,28 @@ VOWEL_LIST = ['A', 'E', 'I', 'O', 'U']
 
 def count_vowel_and_consonent(input_string):
     count = 0
+    num_count = 0
+    if not isinstance(input_string, str) and input_string.isdigit():
+        return 0, 0
+
     for s in input_string:
         if s.upper() in VOWEL_LIST:
             count += 1
+        if s.isdigit():
+            num_count += 1
 
-    return count, int(len(input_string)) - count
+    return count, (int(len(input_string)) - count) - num_count
 
 
 def main():
     input_str = input('Enter the string : ')
     input_str = input_str.replace(" ", "")
     vowel_count, consonent_count = count_vowel_and_consonent(input_str)
-    print('\nNumber of vowel in string => %s \n\nNumber of consonent in string => %s' % (vowel_count, consonent_count))
+
+    if vowel_count != 0 and consonent_count != 0:
+        print('\nNumber of vowel in string => %s \n\nNumber of consonent in string => %s' % (vowel_count, consonent_count))
+    else:
+        print('\nYou have enter wrong string or the string enter by you is incorrect')
 
 
 if __name__ == '__main__':
