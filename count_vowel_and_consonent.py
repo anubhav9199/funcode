17ab56d9afc932ce1c4d1f614b8e5421b005bef7
Anubhav Sharma <anubhav.sharma@medcords.com>
2021-07-22 18:43:58 +0530

    fix : fixing code to match and give answer for numbers and special chars

--- count_vowel_and_consonent.py
+++ count_vowel_and_consonent.py
@@ -1,28 +1,46 @@
+import re
+
+
 VOWEL_LIST = ['A', 'E', 'I', 'O', 'U']
+PATTEN = r"[^0-9a-zA-Z ]"
 
 
-def count_vowel_and_consonent(input_string):
+def count_vowel_consonent_number_special_char(input_string):
     count = 0
     num_count = 0
+    special_char = 0
+
     if not isinstance(input_string, str) and input_string.isdigit():
-        return 0, 0
+        for s in input_string:
+            num_count += 1
+        return 0, 0, num_count, 0
 
     for s in input_string:
         if s.upper() in VOWEL_LIST:
             count += 1
+
         if s.isdigit():
             num_count += 1
 
-    return count, (int(len(input_string)) - count) - num_count
+        if re.match(PATTEN, s):
+            special_char += 1
+    
+    result = {
+        "vowels": count,
+        "consonants": (int(len(input_string)) - count) - (num_count + special_char),
+        "numbers": num_count, "specialCharacters": special_char
+    }
+
+    return result
 
 
 def main():
     input_str = input('Enter the string : ')
     input_str = input_str.replace(" ", "")
-    vowel_count, consonent_count = count_vowel_and_consonent(input_str)
+    result_count = count_vowel_consonent_number_special_char(input_str)
 
-    if vowel_count != 0 and consonent_count != 0:
-        print('\nNumber of vowel in string => %s \n\nNumber of consonent in string => %s' % (vowel_count, consonent_count))
+    if result_count:
+        print('\nNumber of vowel in string => %(vowels)s \n\nNumber of consonent in string => %(consonants)s \n\nNumber of numbers in string => %(numbers)s \n\nNumber of special characters in string => %(specialCharacters)s' % result_count)
     else:
         print('\nYou have enter wrong string or the string enter by you is incorrect')
 
